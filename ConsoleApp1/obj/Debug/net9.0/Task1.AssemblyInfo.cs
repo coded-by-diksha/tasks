8a4ab4674d1ea56868cc9fc4c208f2e5ad4877ee
Dikshaaa1212 <ghimirediksha222@gmail.com>
2025-11-05 12:29:40 +0545

    task 3

--- ConsoleApp1/obj/Debug/net9.0/Task1.AssemblyInfo.cs
+++ ConsoleApp1/obj/Debug/net9.0/Task1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Task1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7de644f8ca88eaf30d93dbfa35f03abfbbc7c58a")]
 [assembly: System.Reflection.AssemblyProductAttribute("Task1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Task1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
